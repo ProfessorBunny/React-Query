da488601a200ced3fda070180ae3e1a07518e482
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-10 12:42:54 +0100

    resolving bugs

--- Front End/src/Items.jsx
+++ Front End/src/Items.jsx
@@ -4,18 +4,18 @@ import customFetch from "./utils";
 const Items = ({ items }) => {
   const { isLoading, data } = useQuery({
     queryKey: ["tasks"],
-    queryFn: () => {
-      customFetch.get("/");
+    queryFn: async () => {
+      const { data } = await customFetch.get("/");
+      return data;
     },
   });
-  console.log(data);
 
   if (isLoading) {
     return <p style={{ marginTop: "1rem" }}>Loading....</p>;
   }
   return (
     <div className="items">
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
